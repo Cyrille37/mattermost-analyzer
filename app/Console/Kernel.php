91c45e8ecd66a581400e253853db25a9b2838720
Cyrille Giquello <cyrille@giquello.fr>
2020-01-03 16:08:57 +0000

    add scheduler for "mm:browseServer"

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+	$schedule->command('mm:browseServer')
+		->twiceDaily(1, 13);
     }
 
     /**
